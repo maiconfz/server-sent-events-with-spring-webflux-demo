a15b00c4893d3c372fdfa8caebf3a1d2376d6740
Maiconfz <maiconfz@gmail.com>
2024-09-07 11:30:10 +0100

    Add classes and configuration from Spring Initializr

---
+++ app/src/main/java/io/github/maiconfz/server_sent_events_with_spring_webflux_demo/ServerSentEventsWithSpringWebfluxDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.maiconfz.server_sent_events_with_spring_webflux_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSentEventsWithSpringWebfluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSentEventsWithSpringWebfluxDemoApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/io/github/maiconfz/server_sent_events_with_spring_webflux_demo/ServerSentEventsWithSpringWebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maiconfz.server_sent_events_with_spring_webflux_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSentEventsWithSpringWebfluxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
